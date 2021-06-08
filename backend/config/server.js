a64412a4c28792ab4f71dda52146df9538578c9b
Mikkel Westermann <mikkel.westermann.98@gmail.com>
2021-06-08 21:45:05 +0200

    Create Strapi starter project

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd40160249a93eaaaf2bf9efc8d344855'),
+    },
+  },
+});
