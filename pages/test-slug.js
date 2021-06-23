5031b0e745ea91c7fc336ab7a366b05b072df527
Mikkel Westermann <mikkel.westermann.98@gmail.com>
2021-06-23 10:57:52 +0200

    Add test slug

---
+++ pages/test-slug.js
@@ -0,0 +1,72 @@
+import ErrorPage from "next/error"
+import { getPageData, fetchAPI, getGlobalData } from "utils/api"
+import Sections from "@/components/sections"
+import Seo from "@/components/elements/seo"
+import { useRouter } from "next/router"
+import Layout from "@/components/layout"
+
+// The file is called [[...slug]].js because we're using Next's
+// optional catch all routes feature. See the related docs:
+// https://nextjs.org/docs/routing/dynamic-routes#optional-catch-all-routes
+
+const DynamicPage = ({ sections, metadata, preview, global, pageContext }) => {
+  const router = useRouter()
+
+  // Check if the required data was provided
+  if (!router.isFallback && !sections?.length) {
+    return <ErrorPage statusCode={404} />
+  }
+
+  // Loading screen (only possible in preview mode)
+  if (router.isFallback) {
+    return <div className="container">Loading...</div>
+  }
+
+  return (
+    <Layout global={global} pageContext={pageContext}>
+      {/* Add meta tags for SEO*/}
+      <Seo metadata={metadata} />
+      {/* Display content sections */}
+      <Sections sections={sections} preview={preview} />
+    </Layout>
+  )
+}
+
+export async function getServerSideProps(context) {
+  const { params, locale, locales, defaultLocale, preview = null } = context
+
+  const globalLocale = await getGlobalData(locale)
+
+  // Fetch pages. Include drafts if preview mode is on
+  const pageData = await getPageData(
+    { slug: !params.slug ? [""] : params.slug },
+    locale,
+    preview
+  )
+
+  if (pageData == null) {
+    // Giving the page no props will trigger a 404 page
+    return { props: {} }
+  }
+
+  // We have the required page data, pass it to the page component
+  const { contentSections, metadata, localizations } = pageData
+
+  return {
+    props: {
+      preview,
+      sections: contentSections,
+      metadata,
+      global: globalLocale,
+      pageContext: {
+        slug: pageData.slug,
+        locale: pageData.locale,
+        locales,
+        defaultLocale,
+        localizations,
+      },
+    },
+  }
+}
+
+export default DynamicPage
