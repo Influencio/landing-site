cdc9d31f3fda63e97ae173ac94938031bc91295a
Mikkel Westermann <mikkel.westermann.98@gmail.com>
2021-06-23 11:17:48 +0200

    Try something different

--- slug.sh
+++ slug.sh
@@ -1,7 +1,11 @@
-if [ "$NODE_ENV" == "production" ]
+echo $NODE_ENV
+
+if [[ "$NODE_ENV" == "production" ]]
 then 
+  echo "Running production version"
   rm ./pages/test-slug.js
 else
+  echo "Running development version"
   rm ./pages/[[...slug]].js
   mv ./pages/test-slug.js ./pages/[[...slug]].js
 fi
