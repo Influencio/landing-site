4b1016b6facdac5666dd48f5eb6f8aa23e61c572
Mikkel Westermann <mikkel.westermann.98@gmail.com>
2021-06-24 11:05:06 +0200

    Add before each to row thing

--- components/sections/feature-rows-group.js
+++ components/sections/feature-rows-group.js
@@ -3,52 +3,55 @@ import Image from "../elements/image";
 import Video from "../elements/video";
 import CustomLink from "../elements/custom-link";
 
-const FeatureRowsGroup = ({ data }) => {
+const FeatureRowsGroup = ({ data, beforeEach }) => {
   return (
     <div className="container flex flex-col gap-12 py-12">
       {data.features.map((feature, index) => (
-        <div
-          className={classNames(
-            // Common classes
-            "flex flex-col justify-start md:justify-between md:items-center gap-10",
-            {
-              "lg:flex-row": index % 2 === 0,
-              "lg:flex-row-reverse": index % 2 === 1,
-            }
-          )}
-          key={feature.id}
-        >
-          {/* Text section */}
-          <div className="w-full lg:w-6/12 lg:pr-6 text-lg">
-            <h4 className='text-2xl font-bold'>{feature.title}</h4>
-            <p className="my-3">{feature.description}</p>
-            {
-              feature.link ? 
+        <>
+          {/* Tile */}
+          {beforeEach ? beforeEach(feature, index) : null}
+          {/* Content */}
+          <div
+            className={classNames(
+              // Common classes
+              "flex flex-col justify-start md:justify-between md:items-center gap-10",
+              {
+                "lg:flex-row": index % 2 === 0,
+                "lg:flex-row-reverse": index % 2 === 1,
+              }
+            )}
+            key={feature.id}
+          >
+            {/* Text section */}
+            <div className="w-full lg:w-6/12 lg:pr-6 text-lg">
+              <h4 className="text-2xl font-bold">{feature.title}</h4>
+              <p className="my-3">{feature.description}</p>
+              {feature.link ? (
                 <CustomLink link={feature.link}>
                   <div className="text-blue-600 with-arrow hover:underline">
                     {feature.link.text}
                   </div>
                 </CustomLink>
-              : null
-            }
+              ) : null}
+            </div>
+            {/* Media section */}
+            <div className="w-full sm:9/12 lg:w-4/12 max-h-full">
+              {/* Images */}
+              {feature.media.mime.startsWith("image") && (
+                <Image media={feature.media} className="w-full h-auto" />
+              )}
+              {/* Videos */}
+              {feature.media.mime.startsWith("video") && (
+                <Video
+                  media={feature.media}
+                  className="w-full h-auto"
+                  autoPlay
+                  controls={false}
+                />
+              )}
+            </div>
           </div>
-          {/* Media section */}
-          <div className="w-full sm:9/12 lg:w-4/12 max-h-full">
-            {/* Images */}
-            {feature.media.mime.startsWith("image") && (
-              <Image media={feature.media} className="w-full h-auto" />
-            )}
-            {/* Videos */}
-            {feature.media.mime.startsWith("video") && (
-              <Video
-                media={feature.media}
-                className="w-full h-auto"
-                autoPlay
-                controls={false}
-              />
-            )}
-          </div>
-        </div>
+        </>
       ))}
     </div>
   );
