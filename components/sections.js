323d7cc5c95b49a0a4b18402bbc8849c6306338e
Mikkel Westermann <mikkel.westermann.98@gmail.com>
2021-06-24 10:54:51 +0200

    Add guide to sections

--- components/sections.js
+++ components/sections.js
@@ -8,6 +8,7 @@ import TestimonialsGroup from "@/components/sections/testimonials-group";
 import RichText from "./sections/rich-text";
 import Pricing from "./sections/pricing";
 import LeadForm from "./sections/lead-form";
+import Guide from './sections/guide';
 
 // Map Strapi sections to section components
 const sectionComponents = {
@@ -20,6 +21,7 @@ const sectionComponents = {
   "sections.rich-text": RichText,
   "sections.pricing": Pricing,
   "sections.lead-form": LeadForm,
+  "sections.guide": Guide
 };
 
 // Display a section individually
