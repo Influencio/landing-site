aa696452cebf8252b2c5c766d26d3ba926a762b8
Mikkel Westermann <mikkel.westermann.98@gmail.com>
2021-06-27 16:42:38 +0200

    Add pricing switch

--- components/elements/pricing-content.js
+++ components/elements/pricing-content.js
@@ -8,10 +8,23 @@ const PricingContent = ({ handleSelect, plansToExclude, plans }) => {
 
   return (
     <>
-      <h1 className='text-4xl text-center'>Choose the plan that works best for you</h1>
-
-
-      {/* <div tw='text-lg mt-3'>Monthly <Switch tw='mx-2' onChange={setAnnually} /> Annually <span tw='text-blue-500'>20% off</span></div> */}
+      <div className='flex space-x-4 justify-center items-center mt-6'>
+        <span className='text-lg'>Monthly</span>
+        <Switch
+          checked={annually}
+          onChange={setAnnually}
+          className={`${annually ? 'bg-primary-500' : 'bg-primary-300'}
+            relative inline-flex flex-shrink-0 h-[32px] w-[66px] border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
+        >
+          <span className="sr-only">Use setting</span>
+          <span
+            aria-hidden="true"
+            className={`${annually ? 'translate-x-9' : 'translate-x-0'}
+              pointer-events-none inline-block h-[28px] w-[28px] rounded-full bg-white shadow-lg transform ring-0 transition ease-in-out duration-200`}
+          />
+        </Switch>
+        <span className='text-lg'>Annually</span>
+      </div>
 
       <Plans annually={annually} handleSelect={handleSelect} plansToExclude={plansToExclude} plans={plans} />
     </>
