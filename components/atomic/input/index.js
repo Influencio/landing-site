9ceb3eb46e0f25e554283d3b43141a00a05145b4
Mikkel Westermann <mikkel.westermann.98@gmail.com>
2021-07-17 12:39:24 +0200

    Add password visibility toggle

--- components/atomic/input/index.js
+++ components/atomic/input/index.js
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { AiOutlineEyeInvisible, AiOutlineEye } from 'react-icons/ai'
 
 const Input = React.forwardRef((props, ref) => {
   const { label, type, id, error } = props;
+
+  const originalInputType = type || 'text';
+  const [inputType, setInputType] = useState(type || 'text')
+
   return (
     <div>
       {
         label ? <label htmlFor={id} className='block text-gray-700 text-sm font-bold mb-2'>{label}</label> : null
       }
-      <input ref={ref} {...props} className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${error ? 'border-red-500' : ''}`} type={type || 'text'} />
+      <div className='relative flex items-center'>
+        <input ref={ref} {...props} className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${error ? 'border-red-500' : ''}`} type={inputType} />
+        {
+          originalInputType === 'password' ? (
+            inputType === 'password' ? <AiOutlineEyeInvisible onClick={() => setInputType('text')} class="cursor-pointer absolute right-4 text-gray-600 text-xl" />
+            : <AiOutlineEye onClick={() => setInputType('password')} class="cursor-pointer absolute right-4 text-gray-600 text-xl" />
+          ) : null
+        }
+      </div>
       {
         error ? <p className="text-red-500 text-xs italic">{error.type === 'required' ? `${label} is required` : null}</p> : null
       }
