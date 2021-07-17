5fd7e752be4ee62245994e0b0ab2406ead607738
Mikkel Westermann <mikkel.westermann.98@gmail.com>
2021-07-17 12:07:07 +0200

    Add support for label on input

--- components/atomic/input/index.js
+++ components/atomic/input/index.js
@@ -1,7 +1,14 @@
 import React from 'react'
 
-const Input = React.forwardRef((props, ref) => (
-  <input ref={ref} {...props} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type={props.type || 'text'} />
-))
+const Input = React.forwardRef((props, ref) => {
+  const { label, type, id } = props;
+  return (
+    <div>
+      {
+        label ? <label htmlFor={id} className='block text-gray-700 text-sm font-bold mb-2'>{label}</label> : null
+      }
+      <input ref={ref} {...props} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type={type || 'text'} />
+    </div>
+)})
 
 export default Input;
