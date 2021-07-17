797aaa23e5686d4d500c7bbf4cd767973853c93f
Mikkel Westermann <mikkel.westermann.98@gmail.com>
2021-07-17 12:10:18 +0200

    Add support for errors in input

--- components/atomic/input/index.js
+++ components/atomic/input/index.js
@@ -1,13 +1,16 @@
 import React from 'react'
 
 const Input = React.forwardRef((props, ref) => {
-  const { label, type, id } = props;
+  const { label, type, id, error } = props;
   return (
     <div>
       {
         label ? <label htmlFor={id} className='block text-gray-700 text-sm font-bold mb-2'>{label}</label> : null
       }
-      <input ref={ref} {...props} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type={type || 'text'} />
+      <input ref={ref} {...props} className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${error ? 'border-red-500' : ''}`} type={type || 'text'} />
+      {
+        error ? <p className="text-red-500 text-xs italic">{error.type === 'required' ? `${label} is required` : null}</p> : null
+      }
     </div>
 )})
 
