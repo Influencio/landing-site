6163fe9b269c32b97a966b7579116e28c499d82a
Mikkel Westermann <mikkel.westermann.98@gmail.com>
2021-07-19 17:10:50 +0200

    Move getStaticProps from register page

--- pages/register/index.js
+++ pages/register/index.js
@@ -5,6 +5,9 @@ import textMap from 'utils/text-map';
 import imageMap from 'utils/image-map';
 import Link from "next/link"
 
+import getCustomProps from "utils/custom-page-props";;
+export const getStaticProps = getCustomProps(['register'])
+
 const Index = ({ metadata, global, pageContext }) => {
   
   const shortTexts = textMap(pageContext.texts.shortTexts)
@@ -35,48 +38,5 @@ const Index = ({ metadata, global, pageContext }) => {
     </Layout>
   )
 }
-
-export async function getStaticProps(context) {
-  const { locale, locales, defaultLocale, preview = null } = context
-
-  const globalLocale = await getGlobalData(locale)
-
-  // Fetch pages. Include drafts if preview mode is on
-  const pageData = await getCustomPageData(
-    { slug: ['register'] },
-    locale,
-    preview
-  )
-
-  if (pageData == null) {
-    // Giving the page no props will trigger a 404 page
-    return { props: {} }
-  }
-
-  // We have the required page data, pass it to the page component
-  const { metadata, localizations, backgroundColor=null, shortTexts, longTexts, images } = pageData
-
-  return {
-    props: {
-      preview,
-      metadata,
-      global: globalLocale,
-      pageContext: {
-        slug: pageData.slug,
-        locale: pageData.locale,
-        locales,
-        defaultLocale,
-        localizations,
-        backgroundColor,
-        texts: {
-          shortTexts,
-          longTexts
-        },
-        images
-      },
-    },
-  }
-}
-
 
 export default Index
