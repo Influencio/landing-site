ed83952b1d7e21afff7abaeace1080839039f542
Mikkel Westermann <mikkel.westermann.98@gmail.com>
2021-07-20 18:18:46 +0200

    Add steps example in company

--- pages/register/company.js
+++ pages/register/company.js
@@ -1,9 +1,36 @@
 import React from 'react'
+import Steps from 'components/atomic/step';
+import {BiSelectMultiple, BiDollarCircle} from 'react-icons/bi'
+import {AiOutlineUser, AiOutlineSmile} from 'react-icons/ai'
 
 const Company = () => {
+
+  const steps = [
+    {
+      content: <div>pis</div>,
+      title: 'test',
+      icon: <BiSelectMultiple />
+    },
+    {
+      content: <div>test</div>,
+      title: 'testtesttest',
+      icon: <AiOutlineUser />
+    },
+    {
+      content: <div>test</div>,
+      title: 'testtesttest',
+      icon: <BiDollarCircle />
+    },
+    {
+      content: <div>test</div>,
+      title: 'testtesttest',
+      icon: <AiOutlineSmile />
+    }
+  ]
+
   return (
     <div>
-      Register a company!
+      <Steps steps={steps} />
     </div>
   )
 }
