c1a43f66593f482532d25f8f5f1a2306c582a7af
Mikkel Westermann <mikkel.westermann.98@gmail.com>
2021-07-20 12:09:24 +0200

    Add success page

--- pages/register/success.js
+++ pages/register/success.js
@@ -1,5 +1,7 @@
 import Seo from "@/components/elements/seo"
 import Layout from "@/components/layout"
+import Button from 'components/elements/button-link';
+import { loginUrl } from 'utils/links';
 
 import getCustomProps from "utils/custom-page-props";;
 export const getStaticProps = getCustomProps(['register', 'success'])
@@ -10,8 +12,17 @@ const Success = ({ metadata, global, pageContext }) => {
       {/* Add meta tags for SEO*/}
       <Seo metadata={metadata} />
 
-      <h1 className='title my-32 text-center'>Success!</h1>
-      <h3 className='text-2xl text-center'>Your new account is now ready</h3>
+      <div className='flex flex-col items-center text-center'>
+        <h1 className='title mt-32'>Success!</h1>
+        <h3 className='text-3xl my-6'>Your new account is now ready</h3>
+
+        <div className='text-lg my-3'>You can now login and start using your account</div>
+        <Button
+          button={{url: loginUrl, text: 'Login'}}
+          appearance='dark'
+          compact
+        />
+      </div>
 
     </Layout>
   )
