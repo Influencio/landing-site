9e88b6ca9fb6bfa4448dc43963848ea861b05502
Mikkel Westermann <mikkel.westermann.98@gmail.com>
2021-07-20 12:22:06 +0200

    Hook up success page to strapi

--- pages/register/success.js
+++ pages/register/success.js
@@ -2,22 +2,29 @@ import Seo from "@/components/elements/seo"
 import Layout from "@/components/layout"
 import Button from 'components/elements/button-link';
 import { loginUrl } from 'utils/links';
+import textMap from 'utils/text-map';
+import imageMap from 'utils/image-map';
 
 import getCustomProps from "utils/custom-page-props";;
 export const getStaticProps = getCustomProps(['register', 'success'])
 
 const Success = ({ metadata, global, pageContext }) => {
+  const shortTexts = textMap(pageContext.texts.shortTexts)
+  const images = imageMap(pageContext.images)
   return (
     <Layout global={global} pageContext={pageContext}>
       {/* Add meta tags for SEO*/}
       <Seo metadata={metadata} />
 
       <div className='flex flex-col items-center text-center'>
-        <h1 className='title mt-32'>Success!</h1>
-        <h3 className='text-3xl my-6'>Your new account is now ready</h3>
+        <h1 className='title mt-32'>{shortTexts.registerSuccessTitle}</h1>
+        <h3 className='text-3xl my-6'>{shortTexts.registerSuccessSubtitle}</h3>
 
-        <div className='text-lg my-3'>You can now login and start using your account</div>
+        <img className='my-8 w-full max-w-lg' src={images.registerSuccessImage.url} alt={images.registerSuccessImage.name} />
+
+        <div className='text-lg my-3'>{shortTexts.registerSuccessCTA}</div>
         <Button
+          id='registerSuccess'
           button={{url: loginUrl, text: 'Login'}}
           appearance='dark'
           compact
