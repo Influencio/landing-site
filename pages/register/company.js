8a75e2634154e8623640f3f6b623ddfdd38892a0
Mikkel Westermann <mikkel.westermann.98@gmail.com>
2021-07-21 17:21:53 +0200

    Add basic form

--- pages/register/company.js
+++ pages/register/company.js
@@ -7,6 +7,9 @@ import Layout from "@/components/layout"
 import { getCustomPageData, getPageData, getGlobalData } from "utils/api"
 import PricingContent from '@/components/elements/pricing-content';
 import { useRouter } from 'next/router'
+import { useForm, Controller } from "react-hook-form";
+import Input from 'components/atomic/input';
+import Button from 'components/elements/button';
 
 export const getStaticProps = async context => {
   const { locale, locales, defaultLocale, preview = null } = context
@@ -93,8 +96,8 @@ const Company = ({ metadata, global, pageContext }) => {
       icon: <BiSelectMultiple />,
     },
     {
-      content: <div>Something</div>,
-      title: "testtesttest",
+      content: <RegisterCompany selectedPlan={selectedPlan} changePlan={() => setCurrentStep(0)} />,
+      title: "Register",
       icon: <AiOutlineUser />,
     },
     {
@@ -128,5 +131,211 @@ const Company = ({ metadata, global, pageContext }) => {
     </Layout>
   )
 }
+
+const RegisterCompany = ({ selectedPlan, changePlan }) => {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+    watch,
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+
+  return (
+    <div className="text-center flex flex-col items-center">
+      <h3 className="text-3xl font-bold">
+        Selected plan: {selectedPlan.title}
+      </h3>
+      <p>{selectedPlan?.price}</p>
+      <p
+        className="my-2 text-lg cursor-pointer text-blue-800"
+        onClick={changePlan}
+      >
+        Change plan
+      </p>
+
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="w-full max-w-screen-sm space-y-4 text-left my-4"
+      >
+        <h4 className="text-xl font-bold">Company data</h4>
+        <Controller
+          name="company.name"
+          control={control}
+          defaultValue=""
+          rules={{ required: true }}
+          render={({ field }) => (
+            <Input
+              id="companyName"
+              label="Name"
+              placeholder="Company Name"
+              error={errors?.company?.name}
+              {...field}
+            />
+          )}
+        />
+
+        <Controller
+          name="company.country"
+          control={control}
+          defaultValue=""
+          rules={{ required: true }}
+          render={({ field }) => (
+            <Input
+              id="companyCountry"
+              label="Country"
+              placeholder="eg. Denmark"
+              error={errors?.company?.country}
+              {...field}
+            />
+          )}
+        />
+
+        <Controller
+          name="company.description"
+          control={control}
+          defaultValue=""
+          rules={{ required: true }}
+          render={({ field }) => (
+            <Input
+              id="companyDescription"
+              label="Description"
+              placeholder="eg. We make cool products for cool people"
+              error={errors?.company?.description}
+              {...field}
+            />
+          )}
+        />
+
+        <h4 className="text-xl font-bold">Your data</h4>
+        <Controller
+          name="user.name.givenName"
+          control={control}
+          defaultValue=""
+          rules={{ required: true }}
+          render={({ field }) => (
+            <Input
+              id="givenName"
+              label="First name"
+              placeholder="Given name"
+              error={errors?.user?.name?.givenName}
+              {...field}
+            />
+          )}
+        />
+
+        <Controller
+          name="user.name.familyName"
+          control={control}
+          defaultValue=""
+          rules={{ required: true }}
+          render={({ field }) => (
+            <Input
+              id="familyName"
+              label="Last name"
+              placeholder="Family name"
+              error={errors?.user?.name?.familyName}
+              {...field}
+            />
+          )}
+        />
+
+        <Controller
+          name="user.email"
+          control={control}
+          defaultValue=""
+          rules={{ required: true }}
+          render={({ field }) => (
+            <Input
+              autoComplete="email"
+              id="email"
+              label="E-mail"
+              placeholder="email@example.com"
+              type="email"
+              error={errors?.user?.email}
+              {...field}
+            />
+          )}
+        />
+
+        <Controller
+          name="user.password"
+          control={control}
+          defaultValue=""
+          rules={{ required: true }}
+          render={({ field }) => (
+            <Input
+              autoComplete="new-password"
+              id="password"
+              label="Password"
+              placeholder="••••••••••"
+              type="password"
+              error={errors?.user?.password}
+              {...field}
+            />
+          )}
+        />
+
+        <Controller
+          name="user.confirm"
+          control={control}
+          defaultValue=""
+          rules={{
+            required: true,
+            validate: (value) =>
+              value === watch("password") || "Passwords do not match",
+          }}
+          render={({ field }) => (
+            <Input
+              autoComplete="new-password"
+              id="confirm"
+              label="Confirm Password"
+              placeholder="••••••••••"
+              type="password"
+              error={errors?.user?.confirm}
+              {...field}
+            />
+          )}
+        />
+
+        {false ? (
+          <div
+            className="bg-red-100 border border-red-400 text-red-700 px-4 py-2 rounded relative"
+            role="alert"
+          >
+            <div>
+              Something went wrong when creating your account:{" "}
+              <span className="font-bold">{error.message}</span>
+            </div>
+          </div>
+        ) : null}
+
+        <Button
+          appearance="dark"
+          compact
+          type="submit"
+          // disabled={isLoading}
+          // loading={isLoading}
+        >
+          Register
+        </Button>
+      </form>
+    </div>
+  );
+};
+
+const ValidCompanyName = ({ isNameValid }) => {
+  if (isNameValid === null) return null;
+
+  return !isNameValid ? (
+    <div className="bg-red-200 border-2 border-red-300 text-gray-700 p-2 mt-2 rounded">
+      A company with this name has already been registered
+    </div>
+  ) : (
+    <div className="bg-green-200 border-2 border-green-300 text-green-700 p-2 mt-2 rounded">
+      The name is available!
+    </div>
+  );
+};
 
 export default Company
