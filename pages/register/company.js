dc1fbcad5a9f9f62088be2c3de5de7ac79149da4
Mikkel Westermann <mikkel.westermann.98@gmail.com>
2021-07-21 16:33:11 +0200

    Add select pricing step to company register page

--- pages/register/company.js
+++ pages/register/company.js
@@ -1,25 +1,90 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Steps from 'components/atomic/step';
 import {BiSelectMultiple, BiDollarCircle} from 'react-icons/bi'
 import {AiOutlineUser, AiOutlineSmile} from 'react-icons/ai'
 import Seo from "@/components/elements/seo"
 import Layout from "@/components/layout"
+import { getCustomPageData, getPageData, getGlobalData } from "utils/api"
+import PricingContent from '@/components/elements/pricing-content';
+import { useRouter } from 'next/router'
 
-import getCustomProps from "utils/custom-page-props";;
-export const getStaticProps = getCustomProps(['register', 'company'])
+export const getStaticProps = async context => {
+  const { locale, locales, defaultLocale, preview = null } = context
+
+  const globalLocale = await getGlobalData(locale)
+
+  // Fetch pages. Include drafts if preview mode is on
+  const pageData = await getCustomPageData(
+    { slug: ['register', 'company'] },
+    locale,
+    preview
+  )
+
+  const priceData = await getPageData(
+    { slug: ['pricing'] },
+    locale,
+    preview
+  )
+
+  if (pageData == null) {
+    // Giving the page no props will trigger a 404 page
+    return { props: {} }
+  }
+
+  // We have the required page data, pass it to the page component
+  const { metadata, localizations, backgroundColor=null, shortTexts, longTexts, images } = pageData
+
+  return {
+    props: {
+      preview,
+      metadata,
+      global: globalLocale,
+      pageContext: {
+        slug: pageData.slug,
+        locale: pageData.locale,
+        locales,
+        defaultLocale,
+        localizations,
+        backgroundColor,
+        texts: {
+          shortTexts,
+          longTexts
+        },
+        images,
+        plans: priceData.contentSections.find(section => section.__component === 'sections.pricing').plans
+      },
+    },
+  }
+}
 
 const Company = ({ metadata, global, pageContext }) => {
 
   const [currentStep, setCurrentStep] = useState(0);
+  const [selectedPlan, setSelectedPlan] = useState(null);
+
+  const router = useRouter()
+  
+  useEffect(() => {
+    const { title, price, annually, action } = router.query
+    if (action === 'select-plan' && (currentStep === 0 || currentStep === 1)) {
+      handleSelectPlan(title, price, annually)
+    }
+  }, [router.query])
 
   const handleChange = step => {
     setCurrentStep(step)
   }
 
+  const handleSelectPlan = (title, price, annually) => {
+    const obj = { title, price, annually }
+    setSelectedPlan(obj);
+    setCurrentStep(1)
+  }
+
   const steps = [
     {
-      content: <div>Something</div>,
-      title: 'test',
+      content: <PricingContent plans={pageContext.plans} />,
+      title: 'Select Plan',
       icon: <BiSelectMultiple />
     },
     {
