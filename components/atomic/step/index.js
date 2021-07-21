7ecfc06c220009997328d52ad6f0a1d68da1d102
Mikkel Westermann <mikkel.westermann.98@gmail.com>
2021-07-21 11:20:29 +0200

    Add responsiveness to steps

--- components/atomic/step/index.js
+++ components/atomic/step/index.js
@@ -6,7 +6,7 @@ import { Transition } from "@headlessui/react";
 const Steps = ({ steps, currentStep = 2 }) => {
   return (
     <div className="w-full my-4 pb-4">
-      <div className="w-full flex pb-3">
+      <div className="w-full flex pb-3 flex-col md:flex-row">
         {steps.map((step, i) => (
           <Step
             key={i}
@@ -25,8 +25,8 @@ const Steps = ({ steps, currentStep = 2 }) => {
 const Step = ({ step, currentStep, last = false, index }) => {
   return (
     <>
-      <div className="relative" style={{ minWidth: 100 }}>
-        <div className="flex-1">
+      <div className="relative my-4 flex md:flex-col items-center" style={{ minWidth: 100 }}>
+        <div className="md:flex-1">
           <div
             className={classNames(
               `relative w-16 h-16 mx-auto rounded-full text-lg flex ${
@@ -53,31 +53,25 @@ const Step = ({ step, currentStep, last = false, index }) => {
                   top: -5,
                   right: -5,
                 }}
-                className={classNames(
-                  "absolute h-6 w-6 bg-blue-500 rounded-full text-lg text-white flex justify-center items-center"
-                  // {
-                  //   hidden: index >= currentStep,
-                  // }
-                )}
+                className="absolute h-6 w-6 bg-blue-500 rounded-full text-lg text-white flex justify-center items-center"
               >
                 <GoCheck />
               </div>
             </Transition>
           </div>
         </div>
-        <div className="text-center">{step.title}</div>
+        <div className="text-left ml-3 md:ml-0 md:text-center font-bold text-xl md:text-base">{step.title}</div>
       </div>
 
       {!last ? (
         <div
-          style={{ marginTop: -20 }}
-          className="mx-3 w-full align-center items-center align-middle content-center flex"
+          className="mx-3 w-2 md:w-full align-center items-center align-middle content-center flex md:mt-[-20px] ml-[25px] md:ml-0"
         >
-          <div className="w-full bg-gray-300 rounded items-center align-middle align-center flex-1">
+          <div className="h-4 md:h-2 w-full bg-gray-300 rounded items-center align-middle align-center flex-1">
             <div
               className={`${
                 index < currentStep ? "bg-blue-300" : "bg-gray-300"
-              } text-xs leading-none py-1 text-center text-gray-800 rounded`}
+              } text-xs leading-none py-1 text-center text-gray-800 rounded h-full`}
               style={{ width: "100%" }}
             ></div>
           </div>
