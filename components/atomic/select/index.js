96de8d59d0749daacbfe904c42256c2e4d84a004
Mikkel Westermann <mikkel.westermann.98@gmail.com>
2021-07-22 16:19:37 +0200

    Add border to listbox button

--- components/atomic/select/index.js
+++ components/atomic/select/index.js
@@ -26,7 +26,7 @@ const Select = ({ data, onChange, label, id, width }) => {
         onChange={handleSelect}
       >
         <div className="relative mt-1">
-          <Listbox.Button className="relative w-full py-2 pl-3 pr-10 text-left bg-white rounded-lg shadow-md cursor-default focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-75 focus-visible:ring-white focus-visible:ring-offset-orange-300 focus-visible:ring-offset-2 focus-visible:border-indigo-500 sm:text-sm">
+          <Listbox.Button className="border relative w-full py-2 pl-3 pr-10 text-left bg-white rounded-lg shadow-md cursor-default focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-75 focus-visible:ring-white focus-visible:ring-offset-orange-300 focus-visible:ring-offset-2 focus-visible:border-indigo-500 sm:text-sm">
             <span className="block truncate">{selected?.name || ""}</span>
             <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
               <CgSelect
