e8316394dbce73260f727aa1d351d8ac3227f180
Mikkel Westermann <mikkel.westermann.98@gmail.com>
2021-07-22 16:28:10 +0200

    Add landing page url

--- utils/urls.js
+++ utils/urls.js
@@ -1,5 +1,17 @@
+const getLanding = () => {
+  switch (process.env.NODE_ENV) {
+    case 'production':
+      return 'https://landing.influencio.dk'
+    case 'staging':
+      return 'https://landing.staging.influencio.dk'
+    default:
+      return 'http://localhost:3000'
+  }
+}
+
 export default {
   hydra: 'https://auth.influencio.dk/hydra',
   helios: 'https://auth.influencio.dk/helios',
-  webApp: 'https://influencio.dk'
+  webApp: 'https://influencio.dk',
+  landing: getLanding()
 }
