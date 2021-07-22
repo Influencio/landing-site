ab05eeb59eca73ff8ce6173ba8ae88e6d9ed7b07
Mikkel Westermann <mikkel.westermann.98@gmail.com>
2021-07-22 16:19:21 +0200

    Use rounded lg for input

--- components/atomic/input/index.js
+++ components/atomic/input/index.js
@@ -64,7 +64,7 @@ const Input = React.forwardRef((props, ref) => {
         <input
           ref={ref}
           {...props}
-          className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
+          className={`shadow appearance-none border rounded-lg w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
             error ? "border-red-500" : ""
           }`}
           type={inputType}
