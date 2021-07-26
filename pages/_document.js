835f77ee46e47ab9817618a23ac0f9c5b5f830f0
Mikkel Westermann <mikkel.westermann.98@gmail.com>
2021-07-26 12:33:55 +0200

    Add some style props to document

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,23 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { extractCritical } from '@emotion/server'
 
 export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    const page = await ctx.renderPage()
+    const styles = extractCritical(page.html)
+    return { ...initialProps, ...page, ...styles }
+  }
+
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <style
+            data-emotion-css={this.props.ids.join(' ')}
+            dangerouslySetInnerHTML={{ __html: this.props.css }}
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
