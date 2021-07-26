8fa1e8e1d2dc759a94eff94b37925863dcf5c51c
Mikkel Westermann <mikkel.westermann.98@gmail.com>
2021-07-26 12:34:12 +0200

    Add some form stuff to enterprise page

--- pages/enterprise.js
+++ pages/enterprise.js
@@ -1,4 +1,8 @@
 import React from 'react'
+import Input from "components/atomic/input";
+import Select from "components/atomic/select";
+import Datetime from 'components/atomic/datetime';
+import { useForm, Controller } from "react-hook-form";
 
 const Enterprise = () => {
   const bgColor = '#2B3856'
@@ -24,8 +28,149 @@ const Enterprise = () => {
 }
 
 const ContactUsForm = () => {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors }
+  } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  }
+
   return (
-    <div>Form</div>
+    <div>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="w-full max-w-screen-sm space-y-4 my-4"
+      >
+        <div className='flex w-full md:space-x-8 flex-col space-y-4 md:space-y-0 md:flex-row'>
+          <Controller
+            name="first"
+            control={control}
+            defaultValue=""
+            rules={{ required: true }}
+            render={({ field }) => (
+              <Input
+                id="first"
+                label="First name"
+                placeholder="Given name"
+                error={errors?.first}
+                {...field}
+              />
+            )}
+          />
+
+          <Controller
+            name="last"
+            control={control}
+            defaultValue=""
+            rules={{ required: true }}
+            render={({ field }) => (
+              <Input
+                id="last"
+                label="Last name"
+                placeholder="Family name"
+                error={errors?.last}
+                {...field}
+              />
+            )}
+          />
+        </div>
+
+        <Controller
+          name="email"
+          control={control}
+          defaultValue=""
+          rules={{ required: true }}
+          render={({ field }) => (
+            <Input
+              id="email"
+              label="Work email address"
+              placeholder="name@company.com"
+              error={errors?.email}
+              {...field}
+            />
+          )}
+        />
+
+        <Controller
+          name="phone.number"
+          control={control}
+          defaultValue=""
+          rules={{ required: true }}
+          render={({ field }) => (
+            <Input
+              id="phoneNumber"
+              label="Phone number"
+              error={errors?.phone?.number}
+              {...field}
+            />
+          )}
+        />
+
+        <Controller
+          name="company"
+          control={control}
+          defaultValue=""
+          rules={{ required: true }}
+          render={({ field }) => (
+            <Input
+              id="company"
+              label="Company name"
+              error={errors?.company}
+              {...field}
+            />
+          )}
+        />
+
+        <Controller
+          name="size"
+          control={control}
+          defaultValue=""
+          rules={{ required: true }}
+          render={({ field }) => (
+            <Select
+              id="size"
+              label="Company size"
+              error={errors?.size}
+              data={['1-49', '50-149', '150-249', '250-999', '1000+'].map(s => ({ key: s, value: s, name: s}))}
+              {...field}
+            />
+          )}
+        />
+
+        <Controller
+          name="industry"
+          control={control}
+          defaultValue=""
+          rules={{ required: true }}
+          render={({ field }) => (
+            <Select
+              id="industry"
+              label="Industry"
+              error={errors?.industry}
+              data={['Fashion', 'Beauty & cosmetics', 'Health & Fitness', 'Travel & lifestyle', 'Technology', 'FMCG'].map(s => ({ key: s, value: s, name: s}))}
+              {...field}
+            />
+          )}
+        />
+
+        <Controller
+          name="time"
+          control={control}
+          defaultValue=""
+          rules={{ required: true }}
+          render={({ field }) => (
+            <Datetime
+              id="time"
+              label="Date & time"
+              error={errors?.time}
+              {...field}
+            />
+          )}
+        />
+      </form>
+    </div>
   )
 }
 
