acddbfdfdc15367f156fbe160db31ca987385f51
Mikkel Westermann <mikkel.westermann.98@gmail.com>
2021-07-28 14:54:57 +0200

    Add className prop to layout

--- components/layout.js
+++ components/layout.js
@@ -3,12 +3,12 @@ import Footer from "./elements/footer"
 import NotificationBanner from "./elements/notification-banner"
 import { useState } from "react"
 
-const Layout = ({ children, global, pageContext, disableNavbar=false, disableFooter=false }) => {
+const Layout = ({ children, global, pageContext, disableNavbar=false, disableFooter=false, className }) => {
   const { navbar, footer, notificationBanner } = global
 
   const [bannerIsShown, setBannerIsShown] = useState(true)
   return (
-    <div className="flex flex-col justify-between min-h-screen">
+    <div className="h-full flex flex-col justify-between min-h-screen">
       {/* Aligned to the top */}
       <div className="flex-1" style={{ backgroundColor: pageContext.backgroundColor || 'white' }}>
         {notificationBanner && bannerIsShown && (
@@ -20,7 +20,7 @@ const Layout = ({ children, global, pageContext, disableNavbar=false, disableFoo
         {
           disableNavbar ? null : <Navbar navbar={navbar} pageContext={pageContext} />
         }
-        <div>{children}</div>
+        <div className={`${className || ''}`}>{children}</div>
       </div>
 
       {
