a08c870a51a1bb2c77c6d88c10462ff4700d5ff8
Mikkel Westermann <mikkel.westermann.98@gmail.com>
2021-08-21 11:36:55 +0200

    Add button for skipping payment

--- pages/register/company.js
+++ pages/register/company.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Steps from "components/atomic/step";
-import { BiSelectMultiple, BiDollarCircle, BiSearchAlt } from "react-icons/bi";
+import { BiSelectMultiple, BiDollarCircle } from "react-icons/bi";
 import { AiOutlineUser, AiOutlineSmile } from "react-icons/ai";
 import Seo from "@/components/elements/seo";
 import Layout from "@/components/layout";
@@ -13,8 +13,7 @@ import Select from "components/atomic/select";
 import Button from "components/elements/button";
 import urls from 'utils/urls'
 import { useQuery } from "react-query";
-
-import tw, { css } from 'twin.macro'
+import Redirect from 'components/other/redirect'
 
 export const getStaticProps = async (context) => {
   const { locale, locales, defaultLocale, preview = null } = context;
@@ -186,47 +185,6 @@ const Company = ({ metadata, global, pageContext }) => {
   );
 };
 
-const Redirect = () => {
-  const redirectCooldown = 1000;
-  const router = useRouter();
-
-  useEffect(() => {
-    setTimeout(() => router.push("/register/success"), redirectCooldown);
-  });
-  return (
-    <div className="flex items-center justify-center w-full">
-      <div tw="space-y-2 text-center w-full max-w-sm">
-        <h4 className="text-xl font-bold">Redirecting you</h4>
-        <div className="h-3 mt-2 relative w-12 rounded-full overflow-hidden">
-          <div className="w-full h-full bg-gray-200 absolute"></div>
-          <div
-            className="h-full bg-blue-500 absolute"
-            css={css`
-              @keyframes expandWidth {
-                0% {
-                  width: 0;
-                }
-                50% {
-                  width: 10%;
-                }
-                75% {
-                  width: 85%;
-                }
-                100% {
-                  width: 100%;
-                }
-              }
-              animation: ${redirectCooldown}ms forwards 0s 1 expandWidth;
-            `}
-          />
-        </div>
-        <div onClick={() => router.push("/register/success")}>
-          Stuck? Click here
-        </div>
-      </div>
-    </div>
-  );
-};
 
 const RegisterCompany = ({ selectedPlan, changePlan, onSuccess }) => {
   const [searchTerm, setSearchTerm] = useState('');
@@ -635,6 +593,8 @@ const Pay = ({ taxIdTypes, onSuccess }) => {
           Pay
         </Button>
       </form>
+
+      <div onClick={onSuccess} className='text-center cursor-pointer text-gray-600'>Skip for now</div>
     </div>
   )
 }
