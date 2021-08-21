a21d1069adfb87529b04b6639bf278686fb8edb1
Mikkel Westermann <mikkel.westermann.98@gmail.com>
2021-08-21 10:42:15 +0200

    Add skip payment to query params

--- components/elements/plans.js
+++ components/elements/plans.js
@@ -13,7 +13,7 @@ const Plans = ({ annually, handleSelect, plansToExclude=[], plans }) => {
       monthly: p.price,
       annually: p.priceAnnually,
     },
-    link: p.link || `/register/company?${querystring.stringify({ title: p.name, price: annually ? p.priceAnnually : p.price, annually, action: 'select-plan' })}`,
+    link: p.link || `/register/company?${querystring.stringify({ title: p.name, price: annually ? p.priceAnnually : p.price, annually, action: 'select-plan', skipPayment: p.skipPayment })}`,
     subtitle: p.description,
     benefits: p.features.map(f => f.name),
     isRecommended: p.isRecommended
