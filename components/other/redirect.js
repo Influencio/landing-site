6344b8e02d365d9dfe651349559f41f423d6e8d0
Mikkel Westermann <mikkel.westermann.98@gmail.com>
2021-09-28 14:32:00 -0400

    Remove emotion stuff

--- components/other/redirect.js
+++ components/other/redirect.js
@@ -1,8 +1,6 @@
 import React, { useEffect } from "react";
 import { useRouter } from "next/router";
 
-import { css } from 'twin.macro'
-
 const Redirect = ({ redirectCooldown=1000, title='Redirecting you', stuckText='Stuck? Click here' }) => {
   const router = useRouter();
 
@@ -11,29 +9,15 @@ const Redirect = ({ redirectCooldown=1000, title='Redirecting you', stuckText='S
   }, []);
   return (
     <div className="flex items-center justify-center w-full">
-      <div className="space-y-2 text-center w-full px-8" css={css`max-width: 400px`}>
+      <div className="space-y-2 text-center w-full px-8" style={{maxWidth: 400}}>
         <h4 className="text-xl font-bold">{title}</h4>
         <div className="h-3 mt-2 relative rounded-full overflow-hidden w-full">
           <div className="w-full h-full bg-gray-200 absolute"></div>
           <div
             className="h-full bg-blue-500 absolute"
-            css={css`
-              @keyframes expandWidth {
-                0% {
-                  width: 0;
-                }
-                50% {
-                  width: 10%;
-                }
-                75% {
-                  width: 85%;
-                }
-                100% {
-                  width: 100%;
-                }
-              }
-              animation: ${redirectCooldown}ms forwards 0s 1 expandWidth;
-            `}
+            style={{
+              animation: `${redirectCooldown}ms forwards 0s 1 expandWidth`
+            }}
           />
         </div>
         <div onClick={() => router.push("/register/success")}>
