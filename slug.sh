32f85fa2492daab0dbdba8fe25a843efed5145ae
Mikkel Westermann <mikkel.westermann.98@gmail.com>
2021-09-28 22:13:24 -0400

    Update slug script

--- slug.sh
+++ slug.sh
@@ -6,6 +6,6 @@ then
   rm ./pages/test-slug.js
 else
   echo "Running development version"
-  rm ./pages/[[...slug]].js
-  mv ./pages/test-slug.js ./pages/[[...slug]].js
+  rm ./pages/[...slug].js
+  mv ./pages/test-slug.js ./pages/[...slug].js
 fi
