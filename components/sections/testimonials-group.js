c224c3dc9d5e4fd37253527804851ba303fda6d5
Mikkel Westermann <mikkel.westermann.98@gmail.com>
2021-09-28 21:55:39 -0400

    Redesign testimonial group

--- components/sections/testimonials-group.js
+++ components/sections/testimonials-group.js
@@ -9,21 +9,45 @@ const TestimonialsGroup = ({ data }) => {
   const selectedTestimonial = data.testimonials[selectedTestimonialIndex];
 
   return (
-    <section className="text-center text-lg bg-gray-200 pt-12 pb-16">
-      <h2 className="title mb-4">{data.title}</h2>
+    <section className="text-lg pt-12 pb-16 flex mx-auto max-w-screen-xl justify-around">
+      <div>
+      <h2 className="title">{data.title}</h2>
       <p className="text-gray-700 mb-4">{data.description}</p>
-      <CustomLink link={data.link}>
-        <span className="with-arrow text-blue-700 hover:underline">
-          {data.link.text}
-        </span>
-      </CustomLink>
+      {/* Change selected testimonial (only if there is more than one) */}
+      {data.testimonials.length > 1 && (
+        <div className="flex flex-row gap-4 mt-10">
+          {data.testimonials.map((testimonial, index) => (
+            <button
+              onClick={() => setSelectedTestimonialIndex(index)}
+              className={classNames(
+                // Common classes
+                "rounded-full h-3 w-3",
+                {
+                  "bg-gray-300": index !== selectedTestimonialIndex,
+                  "bg-blue-600": index === selectedTestimonialIndex,
+                }
+              )}
+              key={testimonial.id}
+            ></button>
+          ))}
+        </div>
+      )}
+      {
+        data.link ?
+        <CustomLink link={data.link}>
+          <span className="with-arrow text-blue-700 hover:underline">
+            {data.link.text}
+          </span>
+        </CustomLink> : null
+      }
+      </div>
       {/* Current testimonial card */}
-      <div className="max-w-5xl w-8/12 sm:w-8/12 bg-white shadow-md sm:shadow-xl mx-auto flex flex-col sm:flex-row mt-10 text-left">
+      <div className="max-w-lg w-8/12 sm:w-8/12 bg-white shadow-md sm:shadow-xl rounded-lg flex flex-col items-center text-left">
         <Image
           media={selectedTestimonial.picture}
-          className="w-full md:w-4/12 object-cover flex-shrink-0"
+          className="h-24 w-24"
         />
-        <div className="px-4 py-4 sm:px-12 sm:pt-12 sm:pb-4 flex flex-col justify-between">
+        <div className="px-4 py-4 sm:px-12 sm:pt-12 sm:pb-4 flex flex-col justify-between relative">
           <div>
             <Image
               media={selectedTestimonial.logo}
@@ -36,35 +60,27 @@ const TestimonialsGroup = ({ data }) => {
             <p className="text-base sm:text-sm">
               {selectedTestimonial.authorTitle}
             </p>
+
+            {
+              data?.testimonials?.length ?
+              <div className='flex absolute space-x-2 right-5 bottom-3'>
+                <div onClick={() => setSelectedTestimonialIndex(Math.abs((selectedTestimonialIndex - 1) % data.testimonials.length))} className='bg-blue-500 text-white rounded-full flex justify-center items-center h-7 w-7 cursor-pointer'>{"<"}</div>
+                <div onClick={() => setSelectedTestimonialIndex((selectedTestimonialIndex + 1) % data.testimonials.length)} className='bg-black text-white rounded-full flex justify-center items-center h-7 w-7 cursor-pointer'>{">"}</div>
+              </div> : null
+            }
           </div>
+          {
+            selectedTestimonial.link ?
           <CustomLink link={{ url: selectedTestimonial.link, text: '', newTab: false, id: 0 }}>
             <span className="uppercase tracking-wide text-blue-700 hover:underline  with-arrow sm:self-end mt-6 sm:mt-0">
               Read story
             </span>
-          </CustomLink>
+          </CustomLink> : null
+          }
         </div>
       </div>
-      {/* Change selected testimonial (only if there is more than one) */}
-      {data.testimonials.length > 1 && (
-        <div className="flex flex-row gap-4 mt-10 justify-center">
-          {data.testimonials.map((testimonial, index) => (
-            <button
-              onClick={() => setSelectedTestimonialIndex(index)}
-              className={classNames(
-                // Common classes
-                "rounded-full h-3 w-3",
-                {
-                  "bg-gray-500": index !== selectedTestimonialIndex,
-                  "bg-blue-600": index === selectedTestimonialIndex,
-                }
-              )}
-              key={testimonial.id}
-            ></button>
-          ))}
-        </div>
-      )}
       {/* Logos list */}
-      <div className="flex flex-row flex-wrap items-center gap-6 sm:gap-20 justify-center mt-10 px-6 sm:px-0">
+      {/* <div className="flex flex-row flex-wrap items-center gap-6 sm:gap-20 justify-center mt-10 px-6 sm:px-0">
         {data.logos.map((logo) => (
           <Image
             media={logo.logo}
@@ -72,7 +88,7 @@ const TestimonialsGroup = ({ data }) => {
             key={logo.id}
           />
         ))}
-      </div>
+      </div> */}
     </section>
   );
 };
