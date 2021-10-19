71852c5f0eb05f546316b6a1e9f4db12042f353b
Mikkel Westermann <mikkel.westermann.98@gmail.com>
2021-10-19 17:57:54 -0400

    Point urls at production

--- utils/urls.js
+++ utils/urls.js
@@ -1,7 +1,7 @@
 const getLanding = () => {
   switch (process.env.NODE_ENV) {
     case 'production':
-      return 'https://landing.influencio.dk'
+      return 'https://influenc.io'
     case 'staging':
       return 'https://landing.staging.influencio.dk'
     default:
@@ -12,8 +12,8 @@ const getLanding = () => {
 export default {
   hydra: 'https://auth.influencio.dk/hydra',
   helios: 'https://auth.influencio.dk/helios',
-  auth: 'https://auth.staging.influencio.dk/auth',
-  accounts: 'https://api.staging.influencio.dk/accounts',
+  auth: 'https://auth.influencio.dk/auth',
+  accounts: 'https://api.influencio.dk/accounts',
   tags: 'https://api.influencio.dk/tags',
   webApp: 'https://influencio.dk',
   landing: getLanding()
