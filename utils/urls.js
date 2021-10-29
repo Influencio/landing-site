e483c76abac27d2eccdedd8b51468b1809f7cc3b
Mikkel Westermann <mikkel.westermann.98@gmail.com>
2021-10-29 17:27:36 -0400

    Update urls

--- utils/urls.js
+++ utils/urls.js
@@ -9,12 +9,34 @@ const getLanding = () => {
   }
 }
 
+const getApi = () => {
+  switch (process.env.NODE_ENV) {
+    case 'production':
+      return 'https://api.influenc.io'
+    case 'staging':
+      return 'https://api.staging.influencio.dk'
+    default:
+      return 'http://localhost:3001'
+  }
+}
+
+const getAuth = () => {
+  switch (process.env.NODE_ENV) {
+    case 'production':
+      return 'https://auth.influenc.io'
+    case 'staging':
+      return 'https://auth.staging.influencio.dk'
+    default:
+      return 'http://localhost:3002'
+  }
+}
+
 export default {
   hydra: 'https://auth.influencio.dk/hydra',
   helios: 'https://auth.influencio.dk/helios',
-  auth: 'https://auth.influenc.io/auth',
-  accounts: 'https://api.influenc.io/accounts',
-  accountsOld: 'https://api.influencio.dk/accounts',
+  auth: getAuth() + '/auth',
+  accounts: getApi() + '/accounts',
+  // accountsOld: 'https://api.influencio.dk/accounts',
   tags: 'https://api.influencio.dk/tags',
   webApp: 'https://influencio.dk',
   landing: getLanding()
