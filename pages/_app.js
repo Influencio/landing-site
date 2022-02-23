f8a7f453d4d20845e699f945949bef532cbe8909
Mikkel Westermann <mikkel.westermann.98@gmail.com>
2022-02-23 14:15:23 +0100

    Implement facbook tracking pixel

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
+import { useEffect } from "react"
 import App from "next/app"
 import Head from "next/head"
 import ErrorPage from "next/error"
+import Script from 'next/script'
 import { useRouter } from "next/router"
 import { DefaultSeo } from "next-seo"
 import { getStrapiMedia } from "utils/media"
@@ -9,11 +11,29 @@ import { QueryClient, QueryClientProvider } from 'react-query'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+import * as fbq from '../lib/fpixel'
+
 const queryClient = new QueryClient()
 
 import "@/styles/index.css"
 
 const MyApp = ({ Component, pageProps }) => {
+  const router = useRouter()
+
+  useEffect(() => {
+    // This pageview only triggers the first time (it's important for Pixel to have real information)
+    fbq.pageview()
+
+    const handleRouteChange = () => {
+      fbq.pageview()
+    }
+
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   // Extract the data we need
   const { global } = pageProps
   if (global == null) {
@@ -24,6 +44,24 @@ const MyApp = ({ Component, pageProps }) => {
 
   return (
     <>
+      {/* Global Site Code Pixel - Facebook Pixel */}
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', ${fbq.FB_PIXEL_ID});
+          `,
+        }}
+      />
+
       {/* Favicon */}
       <Head>
         <link rel="shortcut icon" href={getStrapiMedia(global.favicon.url)} />
--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { FB_PIXEL_ID } from '../lib/fpixel'
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -14,6 +15,15 @@ export default class MyDocument extends Document {
           <style
             dangerouslySetInnerHTML={{ __html: this.props.css }}
           />
+
+          <noscript>
+            <img
+              height="1"
+              width="1"
+              style={{ display: 'none' }}
+              src={`https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1`}
+            />
+          </noscript>
         </Head>
         <body>
           <Main />
