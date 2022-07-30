094d071f368d8c3acaf14764f48e6368c85a343a
Mikkel Westermann <mikkel.westermann.98@gmail.com>
2022-07-30 21:35:37 +0200

    Fix link

--- components/elements/plans.js
+++ components/elements/plans.js
@@ -13,7 +13,7 @@ const Plans = ({ annually, handleSelect, plansToExclude=[], plans }) => {
       monthly: p.price,
       annually: p.priceAnnually,
     },
-    link: p.link || `/register/company?${querystring.stringify({ title: p.name, price: annually ? p.priceAnnually : p.price, annually, action: 'select-plan', skipPayment: p.skipPayment })}`,
+    link: (p.link || '/register/company') + `?${querystring.stringify({ title: p.name, price: annually ? p.priceAnnually : p.price, annually, action: 'select-plan', skipPayment: p.skipPayment })}`,
     subtitle: p.description,
     benefits: p.features.map(f => ({name: f.name, inPlan: f.inPlan})),
     isRecommended: p.isRecommended
